c5e9241cdc1447bc27e995a0e5490bba82f1a72c
longjing <longjing@focusmedia.cn>
2018-09-20 20:50:31 +0800

    add Authorization header

--- app.js
+++ app.js
@@ -2,87 +2,46 @@ const Koa = require('koa');
 const app = new Koa();
 const cors = require('koa2-cors');
 const bodyParser = require('koa-bodyparser');
-const router = require('koa-router')();
+const Router = require('koa-router');
+const model = require('koa-oauth-server/node_modules/oauth2-server/examples/memory/model');
 const index = require('./src/servers/index/s1');
+const login = require('./src/servers/login');
+const oauthserver = require('koa-oauth-server')
 
-const {
-  Tracer,
-  BatchRecorder,
-  jsonEncoder: {JSON_V2}
-} = require('zipkin');
-const zipkinMiddleware = require('./src/middleWare/zipkinMiddle');
-const CLSContext = require('zipkin-context-cls');
-const {HttpLogger} = require('zipkin-transport-http');
-
-// Setup the tracer to use http and implicit trace context
-const tracer = new Tracer({
-  ctxImpl: new CLSContext('zipkin'),
-  recorder: new BatchRecorder({
-    logger: new HttpLogger({
-      endpoint: 'http://localhost:3005',
-      jsonEncoder: JSON_V2
-    })
-  }),
-  localServiceName: 'kuma-node' // name of this application
-});
-  
-
-// app.use(zipkinMiddleware({ 
-//   tracer, 
-//   serviceName: 'kuma-node' // name of this application 
-// })); 
-
-
+const router = new Router();
 // const router = new Router({prefix: '/users'}) // 生成路由前缀
 app.use(bodyParser());
 app.use(cors());
 
+app.oauth = oauthserver({
+  model: model,
+  grants: ['password'],
+  debug: true
+});
+
+app.use(app.oauth.authorise());
+
+app.use(async function(ctx, next) {
+  // console.log('headers:', ctx.request.header);
+  console.log('headers:', ctx.req.get('Authorization'));
+  this.body = 'Secret area';
+  this.status = 200;
+  next();
+});
+ 
+
 app.use(async (ctx, next) => {
   console.log('request body:', ctx.request.body);
   await next();
-  // const rt = ctx.response.get('X-Response-Time');
-  // console.log(`${ctx.method} ${ctx.url} - ${rt}`);
 });
 
 router
-  .get('/test', test)
+  .post('/login', login)
+  // .get('/test', test)
   .all('/', index)
-
-  
-  
-// 重定向
-  async function test(ctx, next) {
-    // const routeR = parseRoute(ctx.url)
-    ctx.body = 'in test page'
-    // ctx.redirect('/'); // 重定向设置
-    // ctx.status = 301;
-  }
-  // x-response-time
-  // app.use(async (ctx, next) => {
-  //   const start = Date.now();
-  //   await next();
-  //   const ms = Date.now() - start;
-  //   ctx.set('X-Response-Time', `${ms}ms`);
-  // });
-  
-  // response
-  
-  // app.use(async ctx => {
-  //   ctx.status = 404;
-  //   ctx.message = 'not found'
-  //   ctx.body = {};
-  // });
-
 
 app
   .use(router.routes())
   .use(router.allowedMethods())
-
-    // 执行自定义错误处理逻辑，如集中式日志记录
-// app.on('error', (err, ctx)=> {
-//   ctx.status = 404;
-//   ctx.message = 'not found'
-//   log.error('server error', err, ctx)
-// });
 
 app.listen(3005);
